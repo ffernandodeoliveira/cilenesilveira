51dd89218d32f24da5d3dd2e7682938a56784163
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2021-05-27 23:14:05 -0300

    Change menu

--- js/scripts.js
+++ js/scripts.js
@@ -61,13 +61,13 @@ $(function () {
     });
 
     /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
-        $('.switcher-wrap').toggleClass('active');
-    });
-    $('.color-switcher ul li').click(function () {
-        var color = $(this).attr('data-color');
-        $('#theme-color').attr("href", "css/" + color + ".css");
-        $('.color-switcher ul li').removeClass('active');
-        $(this).addClass('active');
-    });
+    // $('.switcher-trigger').click(function () {
+    //     $('.switcher-wrap').toggleClass('active');
+    // });
+    // $('.color-switcher ul li').click(function () {
+    //     var color = $(this).attr('data-color');
+    //     $('#theme-color').attr("href", "css/" + color + ".css");
+    //     $('.color-switcher ul li').removeClass('active');
+    //     $(this).addClass('active');
+    // });
 });
